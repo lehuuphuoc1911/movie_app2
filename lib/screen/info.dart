0f81240454e6e12884ac246a0ed2a5a820c6d853
Huu Phuoc <lehuuphuocksbk@gmail.com>
2022-08-08 10:36:03 +0700

    test

--- lib/screen/info.dart
+++ lib/screen/info.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:movie_app/data/fullData.dart';
